c6213708dc127021888c32f3d222a02b86f7ae45
Ken Conley <kwc@kwc.org>
2012-02-17 11:30:25 -0800

    0.2.3 marker

--- src/rosrelease/__init__.py
+++ src/rosrelease/__init__.py
@@ -30,7 +30,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 from .release_base import ReleaseException
 
