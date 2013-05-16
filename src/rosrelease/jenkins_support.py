f97bd4e1e8e72560d4425733017a147a09754e72
Tully Foote <tfoote@osrfoundation.org>
2013-05-16 16:36:01 -0700

    switching to use new server url

--- src/rosrelease/jenkins_support.py
+++ src/rosrelease/jenkins_support.py
@@ -33,7 +33,7 @@
 import os
 import jenkins
 
-SERVER = 'http://build.willowgarage.com/'
+SERVER = 'http://jenkins.willowgarage.com:8080/'
 
 def trigger_source_deb(stack_name, stack_version, distro, simulate=False):
     h = jenkins.Jenkins(SERVER)
@@ -42,7 +42,7 @@ def trigger_source_deb(stack_name, stack_version, distro, simulate=False):
         'STACK_NAME': stack_name,
         'STACK_VERSION': stack_version,        
         }
-    args = ('debbuild-sourcedeb',)
+    args = ('_dry_sourcedeb',)
     kwds = dict(parameters=parameters, token='RELEASE_SOURCE_DEB')
     if simulate:
         return h.build_job_url(*args, **kwds)
--- test/test_jenkins_support.py
+++ test/test_jenkins_support.py
@@ -10,4 +10,4 @@ def test_trigger_source_deb():
     distro = rospkg.distro.load_distro(os.path.join(get_test_path(), 'fuerte.rosdistro'))
     url = trigger_source_deb('common', '1.8.0', distro, simulate=True)
     # not much to this test, mainly tests data structure integration
-    assert url == 'http://build.willowgarage.com/job/debbuild-sourcedeb/buildWithParameters?STACK_NAME=common&STACK_VERSION=1.8.0&token=RELEASE_SOURCE_DEB&DISTRO_NAME=fuerte'
+    assert url == 'http://jenkins.willowgarage.com:8080/job/_dry_sourcedeb/buildWithParameters?STACK_NAME=common&STACK_VERSION=1.8.0&token=RELEASE_SOURCE_DEB&DISTRO_NAME=fuerte'
